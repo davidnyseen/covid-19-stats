d08b816f43c929a9445c6101c33fb6d93de3175a
david nyssen <dyn1565@gmail.com>
2021-08-23 20:45:25 +0300

    added api call to get covid cases in israel

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
-console.log("hello world");
+var http = require('https');
 
-var friends = ["avi", "dani"];
-console.log(friends);
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+  host: 'api.covid19api.com',
+  port: 443,
+  path: '/dayone/country/israel/status/confirmed'
+  
+};
 
-console.log("hello david");
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been received, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been received, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
