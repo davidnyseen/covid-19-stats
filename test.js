e6dfe529e0ce81b88acde92a5fe2f38cdbaa0527
david nyssen <dyn1565@gmail.com>
2021-08-30 21:32:39 +0300

    added server that returns confirmed cases inisrael

--- test.js
+++ test.js
@@ -1,15 +1,28 @@
 var http = require('https');
+const express = require('express');
+
+// express app
+const app = express();
+
+app.listen(3000); //  i am a server
+
+app.get('/', (req, res) => { //  get info for custmer
+// redirects
+  res.redirect('/israel/confirmed');
+});
+
+app.get('/israel/confirmed', (req, res) => { //  get info for custmer
+  var str = '';
 
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
+var confirmedCasesInIsrael = {
   host: 'api.covid19api.com',
   port: 443,
   path: '/dayone/country/israel/status/confirmed'
   
 };
-
-callback = function(response) {
-  var str = '';
+callback = function(response) { // what to do with unput
+  
 
   //another chunk of data has been received, so append it to `str`
   response.on('data', function (chunk) {
@@ -18,8 +31,13 @@ callback = function(response) {
 
   //the whole response has been received, so we just print it out here
   response.on('end', function () {
-    console.log(str);
-  });
+    res.send(str);
+    });
 }
 
-http.request(options, callback).end();
+http.request(confirmedCasesInIsrael, callback).end(); // do options 
+//https://documenter.getpostman.com/view/10808728/SzS8rjbc
+
+  //res.sendFile('./views/index.html', { root: __dirname});
+  
+});
